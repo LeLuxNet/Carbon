357f97a23b9baa07db85347adddc2e2399f67fa1
ginnyTheCat <ginnythecat@lelux.net>
2020-11-15 20:31:03 +0100

    Allow properties for int literals

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -321,7 +321,7 @@ func (l *Lexer) number() token.Token {
 		l.Column++
 	}
 
-	if !l.isEnd() && l.Chars[l.Position] == '.' {
+	if !l.isEnd() && l.Chars[l.Position] == '.' && (len(l.Chars) <= l.Position+1 || l.Chars[l.Position+1] == '.') {
 		l.Position++
 		l.Column++
 
